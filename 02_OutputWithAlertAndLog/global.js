ed2b5717b6f28b0d4d6ddae0ccfccc607064ea08
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-02 22:06:54 -0600

    completed 02_OutputWithAlertAndLog and 03_Variables

---
+++ 02_OutputWithAlertAndLog/global.js
@@ -0,0 +1,22 @@
+// Greeting alert popup
+alert("Hi, I'm the Architect");
+
+//Answers printed to console
+console.log("My favorite breakfast is the fastest option");
+console.log("I own 2 cats");
+console.log("naps are a luxary the architect can't afford");
+console.log("If I could take naps, I wouldn't because they mess up my sleep cycle");
+
+//goodbye alert popup
+alert("That's my time, good-bye");
+
+//bonus
+alert( "1 + 1");//String printed out, no Number operation taking place
+alert( 1 + 1 );//Simple Number addition
+alert( "1" + "1" );//concen....cons....Jamming two strings together ("11")
+alert( "1" + 1 );//returns a string, interesting....
+alert( 1 + "1" );
+var test = ("test" + 1);//it appears that in this situation the Number is treated as a string and concewhatever'ed onto the end of "test" ("test1" is the return)
+
+var a = "1" + 1,
+    b = 1 + "1";
---
+++ 03_Variables/global.js
@@ -0,0 +1,17 @@
+var name,
+    age,
+    hairColor;
+    
+// alert("My name is " + name);
+// alert("My age is " + age);
+// alert("My hair color is " + hairColor);
+
+
+
+console.log("My name is " + name)
+console.log("My age is " + age);
+console.log("My hair color is " + hairColor);
+
+name = "Stevie"
+console.log("My name is " + name)
+
