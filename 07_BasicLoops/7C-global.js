be7b014bcbd93f80205495ecfd1e791207f86248
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-04 13:15:21 -0600

    added bottles of beer song

---
+++ 07_BasicLoops/7C-global.js
@@ -0,0 +1,7 @@
+var bottlesOfBeer = 99;
+
+while (bottlesOfBeer > 0){
+  console.log(bottlesOfBeer + " bottles of beer on a wall blah blah blah");
+  bottlesOfBeer--;
+  console.log("take one blah blah "+bottlesOfBeer+" bottles of beer on the wall")
+} 
