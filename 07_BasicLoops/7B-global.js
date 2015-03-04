9a6582f78cba1c3b479ccf09b27cb7d7c3196c8e
Steven Thiesfeld <scthiesfeld@hotmail.com>
2015-03-04 11:45:52 -0600

    playing around with git

--- 07_BasicLoops/7B-global.js
+++ 07_BasicLoops/7B-global.js
@@ -1,18 +1,20 @@
 var startNum,
-    endNum;
-    
-startNum = parseInt(prompt("enter start number"), 10);
-endNum = parseInt(prompt("enter end number"), 10);
+    endNum,
+    countBy;
+
+startNum = parseFloat(prompt("enter start number"));
+endNum = parseFloat(prompt("enter end number"), 10);
+countBy = parseFloat(prompt("enter number to count by"), 10);
 
 if(startNum > endNum){
   while(startNum >= endNum){
-    console.log("the current number is " +startNum)
-    startNum--
+    console.log("the current number is " +startNum);
+    startNum-=countBy;
   }
 }else{
   while(startNum <= endNum){
     console.log("the current number is " + startNum);
-    startNum++
+    startNum+=countBy;
   }
 }
 
